9e9668f66cf47fbfa4b98e6a7cc2d08d14327c6a
Phil Howard <phil@gadgetoid.com>
2017-12-20 22:00:00 +0000

    Switched to little-endian

--- packets.py
+++ packets.py
@@ -48,14 +48,14 @@ class AdapterCoord24(Adapter):
     def _decode(self, obj, ctx):
         return (obj - 8388608) / 512.0
 
-CoordX = AdapterCoordX(Int16ub)
-CoordY = AdapterCoordY(Int16ub)
+CoordX = AdapterCoordX(Int16ul)
+CoordY = AdapterCoordY(Int16ul)
 Coord24 = AdapterCoord24(Int24ub)
-Rotation = AdapterRotation(Int16ub)
+Rotation = AdapterRotation(Int16ul)
 
-Speed = AdapterSpeed(Int16ub)
-Accel = AdapterAccel(Int16ub)
-Regen = AdapterRegen(Int16ub)
+Speed = AdapterSpeed(Int16ul)
+Accel = AdapterAccel(Int16ul)
+Regen = AdapterRegen(Int16ul)
 HealthEnergy = AdapterHealthEnergy(Int8ub)
 Text = PascalString(Int8ub, encoding='UTF-8')
 TextBig = PascalString(Int16ul, encoding='UTF-8')
@@ -66,8 +66,8 @@ player = {
         'protocol' / Int8ub,
         'name' / Text,
         'session' / Text,
-        'horizonX' / Int16ub,
-        'horizonY' / Int16ub,
+        'horizonX' / Int16ul,
+        'horizonY' / Int16ul,
         'flag' / Text
     ),
     player_commands['BACKUP']: Struct(
@@ -76,20 +76,20 @@ player = {
     ),
     player_commands['HORIZON']: Struct(
         'command' / Default(PlayerCommands, 'HORIZON'),
-        'horizonX' / Int16ub,
-        'horizonY' / Int16ub,
+        'horizonX' / Int16ul,
+        'horizonY' / Int16ul,
     ),
     player_commands['ACK']: Struct(
         'command' / Default(PlayerCommands, 'ACK')
     ),
     player_commands['PONG']: Struct(
         'command' / Default(PlayerCommands, 'PONG'),
-        'num' / Int32ub
+        'num' / Int32ul
     ),
     player_commands['KEY']: Struct(
         'command' / Default(PlayerCommands, 'KEY'),
-        'seq' / Int32ub,
-        'key' / Int8ub,
+        'seq' / Int32ul,
+        'key' / PlayerKeys,
         'state' / Flag
     ),
     player_commands['COMMAND']: Struct(
@@ -103,7 +103,7 @@ player = {
     ),
     player_commands['WHISPER']: Struct(
         'command' / Default(PlayerCommands, 'WHISPER'),
-        'id' / Int16ub,
+        'id' / Int16ul,
         'text' / Text
     ),
     player_commands['SAY']: Struct(
@@ -116,11 +116,11 @@ player = {
     ),
     player_commands['VOTEMUTE']: Struct(
         'command' / Default(PlayerCommands, 'VOTEMUTE'),
-        'id' / Int16ub
+        'id' / Int16ul
     ),
     player_commands['LOCALPING']: Struct(
         'command' / Default(PlayerCommands, 'LOCALPING'),
-        'auth' / Int32ub
+        'auth' / Int32ul
     )
 }
 
@@ -128,23 +128,23 @@ server = {
     server_commands['LOGIN']: Struct(
         'command' / Default(ServerCommands, 'LOGIN'),
         'success' / Flag,
-        'id' / Int16ub,
-        'team' / Int16ub,
-        'clock' / Int32ub,
+        'id' / Int16ul,
+        'team' / Int16ul,
+        'clock' / Int32ul,
         'token' / Text,
         'type' / Int8ub,
         'room' / Text,
         'players' / PrefixedArray(Int16ul, Struct(
-            'id' / Int16ub,
+            'id' / Int16ul,
             'status' / Int8ub,
             'level' / Int8ub,
             'name' / Text,
             'type' / Int8ub,
-            'team' / Int16ub,
+            'team' / Int16ul,
             'posX' / CoordX,
             'posY' / CoordY,
             'rot' / Rotation,
-            'flag' / Int16ub,
+            'flag' / Int16ul,
             'upgrades' / Int8ub
         ))
     ),
@@ -153,14 +153,14 @@ server = {
     ),
     server_commands['PING']: Struct(
         'command' / Default(ServerCommands, 'PING'),
-        'clock' / Int32ub,
-        'num' / Int32ub
+        'clock' / Int32ul,
+        'num' / Int32ul
     ),
     server_commands['PING_RESULT']: Struct(
         'command' / Default(ServerCommands, 'PING_RESULT'),
-        'ping' / Int16ub,
-        'playerstotal' / Int32ub,
-        'playersgame' / Int32ub
+        'ping' / Int16ul,
+        'playerstotal' / Int32ul,
+        'playersgame' / Int32ul
     ),
     server_commands['ACK']: Struct(
         'command' / Default(ServerCommands, 'ACK')
@@ -176,25 +176,25 @@ server = {
     ),
     server_commands['PLAYER_NEW']: Struct(
         'command' / Default(ServerCommands, 'PLAYER_NEW'),
-        'id' / Int16ub,
+        'id' / Int16ul,
         'status' / Int8ub,
         'name' / Text,
         'type' / Int8ub,
-        'team' / Int16ub,
+        'team' / Int16ul,
         'posX' / CoordX,
         'posY' / CoordY,
         'rot' / Rotation,
-        'flag' / Int16ub,
+        'flag' / Int16ul,
         'upgrades' / Int8ub
     ),
     server_commands['PLAYER_LEAVE']: Struct(
         'command' / Default(ServerCommands, 'PLAYER_LEAVE'),
-        'id' / Int16ub
+        'id' / Int16ul
     ),
     server_commands['PLAYER_UPDATE']: Struct(
         'command' / Default(ServerCommands, 'PLAYER_UPDATE'),
-        'clock' / Int32ub,
-        'id' / Int16ub,
+        'clock' / Int32ul,
+        'id' / Int16ul,
         'keystate' / Int8ub,
         'upgrades' / Int8ub,
         'posX' / Coord24,
@@ -205,12 +205,12 @@ server = {
     ),
     server_commands['PLAYER_FIRE']: Struct(
         'command' / Default(ServerCommands, 'PLAYER_FIRE'),
-        'clock' / Int32ub,
-        'id' / Int16ub,
+        'clock' / Int32ul,
+        'id' / Int16ul,
         'energy' / HealthEnergy,
         'energyRegem' / Regen,
         'projectiles' / PrefixedArray(Int8ub, Struct(
-            'id' / Int16ub,
+            'id' / Int16ul,
             'type' / Int8ub,
             'posX' / CoordX,
             'posY' / CoordY,
@@ -222,12 +222,12 @@ server = {
         ))
     ),
     #server_commands['PLAYER_SAY']: Struct(
-    #    'id' / Int16ub,
+    #    'id' / Int16ul,
     #    'text' / Text
     #),
     server_commands['PLAYER_RESPAWN']: Struct(
         'command' / Default(ServerCommands, 'PLAYER_RESPAWN'),
-        'id' / Int16ub,
+        'id' / Int16ul,
         'posX' / Coord24,
         'posY' / Coord24,
         'rot' / Rotation,
@@ -235,32 +235,32 @@ server = {
     ),
     server_commands['PLAYER_FLAG']: Struct(
         'command' / Default(ServerCommands, 'PLAYER_FLAG'),
-        'id' / Int16ub,
-        'flag' / Int16ub
+        'id' / Int16ul,
+        'flag' / Int16ul
     ),
     server_commands['PLAYER_HIT']: Struct(
         'command' / Default(ServerCommands, 'PLAYER_HIT'),
-        'id' / Int16ub,
+        'id' / Int16ul,
         'type' / Int8ub,
         'posX' / CoordX,
         'posY' / CoordY,
-        'owner' / Int16ub,
+        'owner' / Int16ul,
         'players' / PrefixedArray(Int8ub, Struct(
-            'id' / Int16ub,
+            'id' / Int16ul,
             'health' / HealthEnergy,
             'healthRegen' / Regen
         ))
     ),
     server_commands['PLAYER_KILL']: Struct(
         'command' / Default(ServerCommands, 'PLAYER_KILL'),
-        'id' / Int16ub,
-        'killer' / Int16ub,
+        'id' / Int16ul,
+        'killer' / Int16ul,
         'posX' / CoordX,
         'posY' / CoordY
     ),
     server_commands['PLAYER_UPGRADE']: Struct(
         'command' / Default(ServerCommands, 'PLAYER_UPGRADE'),
-        'upgrades' / Int16ub,
+        'upgrades' / Int16ul,
         'type' / Int8ub,
         'speed' / Int8ub,
         'defense' / Int8ub,
@@ -269,17 +269,17 @@ server = {
     ),
     server_commands['PLAYER_TYPE']: Struct(
         'command' / Default(ServerCommands, 'PLAYER_TYPE'),
-        'id' / Int16ub,
+        'id' / Int16ul,
         'type' / Int8ub
     ),
     server_commands['PLAYER_POWERUP']: Struct(
         'command' / Default(ServerCommands, 'PLAYER_POWERUP'),
         'type' / Int8ub,
-        'duration' / Int32ub
+        'duration' / Int32ul
     ),
     server_commands['PLAYER_LEVEL']: Struct(
         'command' / Default(ServerCommands, 'PLAYER_LEVEL'),
-        'id' / Int16ub,
+        'id' / Int16ul,
         'type' / Int8ub,
         'level' / Int8ub
     ),
@@ -287,7 +287,7 @@ server = {
         'command' / Default(ServerCommands, 'GAME_FLAG'),
         'type' / Int8ub,
         'flag' / Int8ub,
-        'id' / Int16ub,
+        'id' / Int16ul,
         'posX' / CoordX,
         'posY' / CoordY,
         'blueteam' / Int8ub,
@@ -295,11 +295,11 @@ server = {
     ),
     server_commands['GAME_SPECTATE']: Struct(
         'command' / Default(ServerCommands, 'GAME_SPECTATE'),
-        'id' / Int16ub
+        'id' / Int16ul
     ),
     server_commands['GAME_PLAYERSALIVE']: Struct(
         'command' / Default(ServerCommands, 'GAME_PLAYERSALIVE'),
-        'players' / Int16ub
+        'players' / Int16ul
     ),
     server_commands['GAME_FIREWALL']: Struct(
         'command' / Default(ServerCommands, 'GAME_FIREWALL'),
@@ -312,8 +312,8 @@ server = {
     ),
     server_commands['EVENT_REPEL']: Struct(
         'command' / Default(ServerCommands, 'EVENT_REPEL'),
-        'clock' / Int32ub,
-        'id' / Int16ub,
+        'clock' / Int32ul,
+        'id' / Int16ul,
         'posX' / CoordX,
         'posY' / CoordY,
         'rot' / Rotation,
@@ -322,7 +322,7 @@ server = {
         'energy' / HealthEnergy,
         'energyRegen' / Regen,
         'players' / PrefixedArray(Int8ub, Struct(
-            'id' / Int16ub,
+            'id' / Int16ul,
             'keystate' / Int8ub,
             'posX' / CoordX,
             'posY' / CoordY,
@@ -335,7 +335,7 @@ server = {
             'playerHealthRegen' / Regen
         )),
         'mobs' / PrefixedArray(Int8ub, Struct(
-            'id' / Int16ub,
+            'id' / Int16ul,
             'type' / Int8ub,
             'posX' / CoordX,
             'posY' / CoordY,
@@ -348,8 +348,8 @@ server = {
     ),
     server_commands['EVENT_BOOST']: Struct(
         'command' / Default(ServerCommands, 'EVENT_BOOST'),
-        'clock' / Int32ub,
-        'id' / Int16ub,
+        'clock' / Int32ul,
+        'id' / Int16ul,
         'boost' / Flag,
         'posX' / CoordX,
         'posY' / CoordY,
@@ -361,8 +361,8 @@ server = {
     ),
     server_commands['EVENT_BOUNCE']: Struct(
         'command' / Default(ServerCommands, 'EVENT_BOUNCE'),
-        'clock' / Int32ub,
-        'id' / Int16ub,
+        'clock' / Int32ul,
+        'id' / Int16ul,
         'keystate' / Int8ub,
         'posX' / CoordX,
         'posY' / CoordY,
@@ -372,7 +372,7 @@ server = {
     ),
     server_commands['EVENT_STEALTH']: Struct(
         'command' / Default(ServerCommands, 'EVENT_STEALTH'),
-        'id' / Int16ub,
+        'id' / Int16ul,
         'state' / Flag,
         'energy' / HealthEnergy,
         'energyRegen' / Regen,
@@ -380,12 +380,12 @@ server = {
     server_commands['EVENT_LEAVEHORIZON']: Struct(
         'command' / Default(ServerCommands, 'EVENT_LEAVEHORIZON'),
         'type' / Int8ub,
-        'id' / Int16ub
+        'id' / Int16ul
     ),
     server_commands['MOB_UPDATE']: Struct(
         'command' / Default(ServerCommands, 'MOB_UPDATE'),
-        'clock' / Int32ub,
-        'id' / Int16ub,
+        'clock' / Int32ul,
+        'id' / Int16ul,
         'type' / Int8ub,
         'posX' / CoordX,
         'posY' / CoordY,
@@ -397,41 +397,41 @@ server = {
     ),
     server_commands['MOB_UPDATE_STATIONARY']: Struct(
         'command' / Default(ServerCommands, 'MOB_UPDATE_STATIONARY'),
-        'id' / Int16ub,
+        'id' / Int16ul,
         'type' / Int8ub,
         'posX' / Float32b,
         'posY' / Float32b
     ),
     server_commands['MOB_DESPAWN']: Struct(
         'command' / Default(ServerCommands, 'MOB_DESPAWN'),
-        'id' / Int16ub,
+        'id' / Int16ul,
         'type' / Int8ub
     ),
     server_commands['MOB_DESPAWN_COORDS']: Struct(
         'command' / Default(ServerCommands, 'MOB_DESPAWN_COORDS'),
-        'id' / Int16ub,
+        'id' / Int16ul,
         'type' / Int8ub,
         'posX' / CoordX,
         'posY' / CoordY
     ),
     server_commands['SCORE_UPDATE']: Struct(
         'command' / Default(ServerCommands, 'SCORE_UPDATE'),
-        'id' / Int16ub,
-        'score' / Int32ub,
-        'earnings' / Int32ub,
-        'upgrades' / Int16ub,
-        'totalkills' / Int32ub,
-        'totaldeaths' / Int32ub
+        'id' / Int16ul,
+        'score' / Int32ul,
+        'earnings' / Int32ul,
+        'upgrades' / Int16ul,
+        'totalkills' / Int32ul,
+        'totaldeaths' / Int32ul
     ),
     server_commands['SCORE_BOARD']: Struct(
         'command' / Default(ServerCommands, 'SCORE_BOARD'),
         'data' / PrefixedArray(Int8ub, Struct(
-            'id' / Int16ub,
-            'score' / Int32ub,
+            'id' / Int16ul,
+            'score' / Int32ul,
             'level' / Int8ub
         )),
         'rankings' / PrefixedArray(Int8ub, Struct(
-            'id' / Int16ub,
+            'id' / Int16ul,
             'x' / Int8ub,
             'y' / Int8ub
         ))
@@ -439,53 +439,53 @@ server = {
     server_commands['SCORE_DETAILED']: Struct(
         'command' / Default(ServerCommands, 'SCORE_DETAILED'),
         'scores' / PrefixedArray(Int8ub, Struct(
-            'id' / Int16ub,
+            'id' / Int16ul,
             'level' / Int8ub,
-            'score' / Int32ub,
-            'kills' / Int16ub,
-            'deaths' / Int16ub,
+            'score' / Int32ul,
+            'kills' / Int16ul,
+            'deaths' / Int16ul,
             'damage' / Float32b,
-            'ping' / Int16ub
+            'ping' / Int16ul
         ))
     ),
     server_commands['SCORE_DETAILED_CTF']: Struct(
         'command' / Default(ServerCommands, 'SCORE_DETAILED_CTF'),
         'scores' / PrefixedArray(Int8ub, Struct(
-            'id' / Int16ub,
+            'id' / Int16ul,
             'level' / Int8ub,
             'alive' / Flag,
-            'wins' / Int16ub,
-            'score' / Int32ub,
-            'kills' / Int16ub,
-            'deaths' / Int16ub,
+            'wins' / Int16ul,
+            'score' / Int32ul,
+            'kills' / Int16ul,
+            'deaths' / Int16ul,
             'damage' / Float32b,
-            'ping' / Int16ub
+            'ping' / Int16ul
         ))
     ),
     server_commands['CHAT_TEAM']: Struct(
         'command' / Default(ServerCommands, 'CHAT_TEAM'),
-        'id' / Int16ub,
+        'id' / Int16ul,
         'text' / Text
     ),
     server_commands['CHAT_PUBLIC']: Struct(
         'command' / Default(ServerCommands, 'CHAT_PUBLIC'),
-        'id' / Int16ub,
+        'id' / Int16ul,
         'text' / Text
     ),
     server_commands['CHAT_SAY']: Struct(
         'command' / Default(ServerCommands, 'CHAT_SAY'),
-        'id' / Int16ub,
+        'id' / Int16ul,
         'text' / Text
     ),
     server_commands['CHAT_WHISPER']: Struct(
         'command' / Default(ServerCommands, 'CHAT_WHISPER'),
-        'from' / Int16ub,
-        'to' / Int16ub,
+        'from' / Int16ul,
+        'to' / Int16ul,
         'text' / Text
     ),
     server_commands['CHAT_VOTEMUTEPASSED']: Struct(
         'command' / Default(ServerCommands, 'CHAT_VOTEMUTEPASSED'),
-        'id' / Int16ub
+        'id' / Int16ul
     ),
     server_commands['CHAT_VOTEMUTED']: Struct(
         'command' / Default(ServerCommands, 'CHAT_VOTEMUTED')
@@ -493,7 +493,7 @@ server = {
     server_commands['SERVER_MESSAGE']: Struct(
         'command' / Default(ServerCommands, 'SERVER_MESSAGE'),
         'type' / Int8ub,
-        'duration' / Int32ub,
+        'duration' / Int32ul,
         'message' / TextBig
     ),
     server_commands['SERVER_CUSTOM']: Struct(
@@ -509,6 +509,6 @@ def build_player_command(command, contents):
 
 def decode_server_command(command):
     id = ord(command[0])
-    name = server_commands.keys()[server_commands.values().index(id)]
-    print("Parsing Command ID: {}, Name: {}".format(id, name))
+    #name = server_commands.keys()[server_commands.values().index(id)]
+    #print("Parsing Command ID: {}, Name: {}".format(id, name))
     return server[id].parse(command)
